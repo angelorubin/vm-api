809416d5671d00c277b578b6a353e84344aa7013
angelorubin <angelorubin@gmail.com>
2017-03-22 18:10:51 -0300

    modificando res.status por res.json

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use( (err, req, res, next) => {
-        res.status(err.status || 500)
+        res.json(err.status || 500)
         res.json('error', {
             message: err.message,
             error: err
